8c4360cf718a36031a3dacfbc8688b6537951577
rogelio1502 <rogelio15021515@gmail.com>
2023-02-07 18:30:18 -0600

    v0.0.49

--- src/EF/Providers/EfServiceProvider.php
+++ src/EF/Providers/EfServiceProvider.php
@@ -3,12 +3,14 @@
 namespace EF\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Orchid\Platform\Dashboard;
 class EfServiceProvider extends ServiceProvider
 {
 
-    public function boot()
+    public function boot(Dashboard $ds)
     {
+        $ds->registerResource('scripts','/js/app.js');
+        $ds->registerResource('stylesheets','/css/tom-select.default.css');
         $this->loadViewsFrom(__DIR__ . '/../../../resources/views', 'ef');
     }
     
